cbb1208265bc730e828a82841d638ce701754023
DevArthurBarbiza <arthur.barboza0016@gmail.com>
2026-01-07 10:28:02 -0300

    ✨ Create list with remove and add features

--- src/List.tsx
+++ src/List.tsx
@@ -1,20 +1,38 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 export default function List(){
 
-    const elements = ['Item 1', 'Item 2', 'Item 3'];
-    const formattedElements = elements.map(element =>
-        <li>
-            element
-        </li>
-    )
+    const [elements, setElements] = useState(['Item 1', 'Item 2', 'Item 3']);
+    const [newItem, setNewItem] = useState('');
+
+    function addElement(){
+      elements.push(newItem)
+      setElements(elements)
+      setNewItem('');
+    }
+    
+    function handleItemChange(value : string){
+      setNewItem(value)
+    }
+
+    function handleRemoveItem(value : string) {
+      let newElementList = [...elements]
+      newElementList = newElementList.filter(element => element != value);
+      setElements(newElementList)
+    }
   
     return (
       <>
-      <input placeholder="teste" />
+        <input placeholder="teste" type="text" id="new-element" value={newItem} onChange={e => handleItemChange(e.target.value)}/>
+        <button type="button" onClick={addElement} >Adicionar</button>
         <ul>
-          {formattedElements}
+          {elements.map((element) => (
+              <> 
+                <li> {element} 
+                  <button type="button" onClick={() => handleRemoveItem(element)}>X</button> 
+                </li> 
+              </>
+          ))}
         </ul>
       </>
     )
